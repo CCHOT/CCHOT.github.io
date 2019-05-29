0c8f0b34778536570c28303e8d54528f86507363
mattyu <mattyu@tencent.com>
2019-05-29 21:09:14 +0800

    mod

--- js/json-editor.js
+++ js/json-editor.js
@@ -13,83 +13,27 @@ function init() {
 
 function json_edit_init() {
     JSONEditor.defaults.options.template = 'swig';
-    JSONEditor.defaults.languages.en.button_collapse = "收起";
-    JSONEditor.defaults.languages.en.button_expand = "展开";
-    JSONEditor.defaults.languages.en.button_edit_json = "编辑schema";
-    JSONEditor.defaults.languages.en.button_object_properites = "添加";
-    JSONEditor.defaults.languages.en.button_add_properties = "添加";
+    JSONEditor.defaults.languages.en.button_collapse = "collapse";
+    JSONEditor.defaults.languages.en.button_expand = "expand";
+    JSONEditor.defaults.languages.en.button_edit_json = "edit_schema";
+    JSONEditor.defaults.languages.en.button_object_properites = "add";
+    JSONEditor.defaults.languages.en.button_add_properties = "add";
     JSONEditor.defaults.languages.en.error_minimum_incl = "最少长度为{{0}}";
     JSONEditor.defaults.languages.en.error_pattern = "必须满足正则表达公式{{0}}";
     JSONEditor.defaults.languages.en.error_minLength = "最少长度为{{0}}";
     JSONEditor.defaults.languages.en.error_notempty = "不能为空";
     let schema={
-        "title": "大T JSON Schema",
+        "title": "demo",
         "type":"object",
         "options":{
             "disable_collapse":false,
             "disable_edit_json":false,
         },
         "format":"grid",
-        "required":["properties","op_type","id","type","required","desc","rep_user","app_version"],
+        "required":["properties"],
         "properties": {
-            "op_type":{
-                "type":"string",
-                'valid_format':'op_type',
-                "minLength": 8,
-                "maxLength": 11,
-                "pattern" : "^[0-9A-Z]+$",
-                "options":{
-                    "grid_columns":2
-                }
-                },
-            "rep_user":{
-                "title":"责任人",
-                "type":"string",
-                "minLength":1,
-                "options":{
-                    "grid_columns":2
-                },
-            },
-            "app_version":{
-                "title":"起始APP版本",
-                "type":"string",
-                "minLength":1,
-                "pattern":"^[0-9]{3}$",
-                "options":{
-                    "grid_columns":2
-                },
-                "description":"开始校验的APP版本,例: 800"
-            },
-            "id":{
-                "type":"string",
-                "default":"#",
-                "options":{
-                    "hidden":true
-                }
-                },
-            "type":{
-                "type":"string",
-                "default":"object",
-                "options":{
-                    "hidden":true
-                },
-            },
-            "required":{
-                "type":"array",
-                "options":{
-                    "hidden":true
-                },
-            },
-            "desc":{
-                "type":"string",
-                "title":"备注",
-                "options":{
-                    "grid_columns":6
-                },
-                "description":"请输入大T用途描述信息"
-            },
             "properties": {
-                "title": "列规则定义",
+                "title": "properties",
                 "options":{
                     disable_properties:false,
                     disable_collapse:false,
@@ -119,29 +63,23 @@ function json_edit_init() {
         },
         "definitions": {
             "basic_col":{
-                "required":["id","type","value_set_type","value_set","desc"],
+                "required":["type","value_set_type"],
                 "type":"object",
                 "format":"grid",
                 "properties":{
-                    "id":{
-                        "type":"string",
-                        "options":{
-                            "hidden":true
-                        }
-                        },
                     "type":{
-                        "title":"类型",
+                        "title":"type",
                         "type":"string",
                         "id":"type",
                         "enumSource": [{
                             "source": [
                                 {
                                     "value": "string",
-                                    "title": "字符串"
+                                    "title": "string"
                                 },
                                 {
                                     "value": "integer",
-                                    "title": "整型"
+                                    "title": "integer"
                                 },
                                 {
                                     "value": "double",
@@ -149,14 +87,14 @@ function json_edit_init() {
                                 },
                                 {
                                     "value": "boolean",
-                                    "title": "布尔"
+                                    "title": "boolean"
                                 }],
                             "title": "{{item.title}}",
                             "value": "{{item.value}}"
                         }],
                     },
                     "value_set_type":{
-                        "title":"取值类型",
+                        "title":"value_set_type",
                         "type":"string",
                         "name":'t1',
                         "format":'relative path',
@@ -164,17 +102,17 @@ function json_edit_init() {
                             "source": [
                                 {
                                     "value": "continuous",
-                                    "title": "连续区间",
+                                    "title": "continuous",
                                     "filter":['integer','double']
                                 },
                                 {
                                     "value": "discrete",
-                                    "title": "离散枚举",
+                                    "title": "discrete",
                                     "filter":['integer','string','boolean']
                                 },
                                 {
                                     "value": "pattern",
-                                    "title": "正则表达式",
+                                    "title": "pattern",
                                     "filter":['string']
                                 }],
                             "title": "{{item.title}}",
@@ -185,46 +123,13 @@ function json_edit_init() {
                             type:"type"
                         },
                     },
-                    "value_set":{
-                        "title":"取值规则",
-                        "type":"string",
-                        "valid_format":'value_set',
-                        "description":"<strong>连续区间：</strong>开闭区间字符串,如：(0,12]；<br>" +
-                        '<strong>离散枚举：</strong>array字符串，如 ["体育","娱乐"]；<br>' +
-                        '<strong>正则表达式：</strong>正则表达式字符串，如[0-9]+'
-                    },
-                    "desc":{
-                        "type":"string",
-                        "title":"备注",
-                        "description":"请输入列含义的描述信息"
-                    },
                 },
             },
             "complex_col":{
-                "required":["id","type","required","properties"],
+                "required":["properties"],
                 "type":"object",
                 "format":"grid",
                 "properties":{
-                    "id":{
-                        "type":"string",
-                        "options":{
-                            "hidden":true
-                        },
-                    },
-                    "type":{
-                        "type":"string",
-                        "default":"object",
-                        "options":{
-                            "hidden":true
-                        },
-                    },
-                    "required":{
-                        "type":"string",
-                        "watch":"",
-                        "options":{
-                            "hidden":true
-                        },
-                    },
                     "properties": {
                         "type": "object",
                         "title": "子列",
@@ -246,11 +151,11 @@ function json_edit_init() {
                 },
                 "oneOf": [
                     {
-                        "title": "基础列",
+                        "title": "basic_col",
                         "$ref": "#/definitions/basic_col",
                     },
                     {
-                        "title": "组合列",
+                        "title": "complex_col",
                         "$ref": "#/definitions/complex_col",
                     }
                 ],
